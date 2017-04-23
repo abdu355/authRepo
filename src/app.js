e8a52cad48739abe8ca3bce0ef5ef6c45c74856d
abdu355 <abdu_sah@hotmail.com>
2017-04-24 01:22:54 +0400

    full initial commit

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyAlXDsbDWw7FdswC2VlduS30ffXign7CoE',
+    authDomain: 'auth-95aa2.firebaseapp.com',
+    databaseURL: 'https://auth-95aa2.firebaseio.com',
+    projectId: 'auth-95aa2',
+    storageBucket: 'auth-95aa2.appspot.com',
+    messagingSenderId: '521270367050'
+  });
+}
+render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <LoginForm />
+      </View>
+    );
+  }
+}
+export default App;
---
+++ src/components/common/index.js
@@ -0,0 +1,6 @@
+//import and export in one line
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
